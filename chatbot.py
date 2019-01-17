3ac292ddc682dd6d65583c36bf5e451d88dd3827
Szymon Słomiński <simon.slominski@gmail.com>
2019-01-17 11:13:54 +0100

    Update chatbot.py by import the dataset

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,3 @@
-
 # Importing the libraries
 import numpy as np
 import tensorflow as tf
@@ -8,4 +7,6 @@ import re
 
 # Part 1 - Data Preprocessing
 
-
+# 1.1 Importing the dataset
+lines = open('movie_lines.txt', encoding = 'UTF-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'UTF-8', errors = 'ignore').read().split('\n')
