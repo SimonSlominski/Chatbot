ad1d0167ea65146d126af14af1946765a6737ccc
Szymon Słomiński <simon.slominski@gmail.com>
2019-01-19 17:25:00 +0100

    Update chatbot.py by separate Q&A

--- chatbot.py
+++ chatbot.py
@@ -11,3 +11,48 @@ import re
 # Check if you are in your working directory (pwd in Spider's console)
 lines = open('movie_lines.txt', encoding = 'UTF-8', errors = 'ignore').read().split('\n')
 conversations = open('movie_conversations.txt', encoding = 'UTF-8', errors = 'ignore').read().split('\n')
+
+
+# 1.2 Create dictionary to map each line and its ID
+id2line = {}
+
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    # Check if, in fact, the _line variable contains 5 elements
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+        
+        
+# 1.3 Create a list of all of the conversations
+conversations_ids = []
+
+for conversation in conversations[:-1]:
+    # Take last element from the list and remove square brackets, quotes
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+    
+    
+# 1.4 Getting separately the questions and the answers
+questions = []
+answers = []
+
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
