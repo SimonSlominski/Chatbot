a3b2371eb512f9df4b61379d23f6bd6448a58b8e
Szymon Słomiński <simon.slominski@gmail.com>
2019-02-27 22:14:34 +0100

    Split data into batches of Q&A

--- chatbot.py
+++ chatbot.py
@@ -405,6 +405,17 @@ def apply_padding(batch_of_sequences, word2int):
     return [sequence + [word2int['<PAD>']] * (max_sequence_length - len(sequence)) for sequence in batch_of_sequences]
  
     
+# 3.9 Splitting the data into batches of questions and answers
+def split_into_batches(questions, answers, batch_size):
+    for batch_index in range(0, len(questions) // batch_size):
+        start_index = batch_index * batch_size
+        questions_in_batch = questions[start_index : start_index + batch_size]
+        answers_in_batch = answers[start_index : start_index + batch_size]
+        padded_questions_in_batch = np.array(apply_padding(questions_in_batch, questionswords2int))
+        padded_answers_in_batch = np.array(apply_padding(answers_in_batch, answerswords2int))
+        yield padded_questions_in_batch, padded_answers_in_batch
+ 
+    
 
 
 
