c347a5ef95c1f36233b298ee58c215356e2aa64c
Szymon Słomiński <simon.slominski@gmail.com>
2019-03-01 14:15:10 +0100

    Set up the chat

--- chatbot.py
+++ chatbot.py
@@ -526,19 +526,32 @@ def convert_string2int(question, word2int):
     return [word2int.get(word, word2int['<OUT>']) for word in question.split()]
 
 
+# 4.3 Setting up the chat
+while(True):
+    question = input("You: ")
+    if question == "Goodbye":
+        break
+    question = convert_string2int(question, questionswords2int)
+    question = question + [questionswords2int['<PAD>']] * (20 - len(question))
+    fake_batch = np.zeros((batch_sieze, 20))
+    fake_batch[0] = question
+    predicted_answer = session.run(test_predictions, {inputs: fake_batch,
+                                                      keep_prob: 0.5})[0]
+    answer = ''
+    for i in np.argmax(predicted_answer, 1):
+        if answersints2word[i] == 'i':
+            token = ' I'
+        elif answersints2word[i] == '<EOS>':
+            token = '.'
+        elif answersints2word[i] == '<OUT>':
+            token = 'out'
+        else:
+            token = ' ' + answersints2word[i]
+        answer += token
+        if token == '.':
+            break
+    print('ChatBot: ' + answer)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# To run the chatbot, run all code without "training part" = run Part 1, 2, 4.
 
